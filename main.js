786c4a494b235166b9781ecf605329b2c54cf7ee
Doublepicebs <nananny2005@gmail.com>
2019-12-24 21:30:29 +0200

    js layer

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
+/*
+Make as aroma js?
+*/
+
+
+
 let bur = document.getElementsByClassName("arc-navbar-burger");
 let burger = bur[0];
+let dra = document.getElementsByClassName("arc-drawer");
+let drawer = dra[0];
+let bas = document.getElementsByClassName("arc-basic");
+let basic = bas[0];
+
 console.log(burger);
-let c = false;
+console.log(drawer);
+console.log(basic);
+
+var c = false;
+
+
+drawer.style.display = "none";
+drawer.style.opacity = "0%";
 
 function arcBurgerClicker() {
   if (c === false) {
@@ -16,8 +34,19 @@ function arcBurgerClicker() {
 }
 
 function openDrawer() {
-  
+  basic.style.display = "none";
+  drawer.style.display = "block";
+  drawer.style.opacity = "100%";
+  burger.text = "Х";
+  console.log(basic.style.display);
+  console.log(drawer.style.display);
 }
+
 function closeDrawer() {
-  
+  basic.style.display = "block";
+  drawer.style.opacity = "0%";
+  drawer.style.display = "none";
+  burger.text = "=";
+  console.log(basic.style.display);
+  console.log(drawer.style.display);
 }
